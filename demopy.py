9afac53035e7ac90aa6fd2c13d25feacae272421
PriyaRaviit3 <34087852+PriyaRaviit3@users.noreply.github.com>
2018-02-14 11:49:57 +0530

    Add files via upload

---
+++ demopy.py
@@ -0,0 +1,220 @@
+
+>>> a=9
+>>> b=5
+>>> a<b
+False
+>>> if(a<b):
+	print("hi")
+
+	
+>>> if(a>b):
+	print("hi")
+
+	
+hi
+>>> if(a<b)&(b<a):
+	print("hi")
+
+	
+>>> if(a>b)&(b<a):
+	print("hi")
+
+	
+hi
+>>> if(a<b):
+	print("hi")
+else:
+	print("hello")
+
+	
+hello
+>>> i=7
+>>> while(i>4):
+	print("hi")
+	break
+
+hi
+>>> while(True):
+	print("hiii")
+	break
+
+hiii
+>>> def __init__(self,x,y)
+
+>>> def __init__(self,x,y):
+	self.x=self.x
+	self.y=self.y
+	tot=self.x+self.y
+
+	
+>>> 3,5
+(3, 5)
+>>> class student:
+
+	def __init__(self,x,y):
+		self.x=self.x
+		self.y=self.y
+		tot=self.x+self.y
+			print("enter the value")
+			
+
+>>> class student:
+
+	def __init__(self,x,y):
+		self.x=self.x
+		self.y=self.y
+		tot=self.x+self.y
+
+			
+>>> stu=student(3,5)
+
+>>> x=9
+>>> y=4
+>>> if(x>y):
+	print("x is greater than y")
+elif(x==9):
+	print("x is equal to 9")
+elif(y==4):
+	print("y is equal to 4")
+
+	
+x is greater than y
+>>> if(x<y):
+	print("x is greater than y")
+elif(x==9):
+	print("x is equal to 9")
+elif(y==4):
+	print("y is equal to 4")
+
+	
+x is equal to 9
+>>> if(x<y):
+	print("x is greater than y")
+elif(x==7):
+	print("x is equal to 9")
+elif(y==4):
+	print("y is equal to 4")
+
+	
+y is equal to 4
+>>> if(x<y):
+	print("x is greater than y")
+elif(x==7):
+	print("x is equal to 9")
+elif(y==3):
+	print("y is equal to 4")
+
+	
+>>> def display(var):
+if(var==0):
+	print("x is equal to zero")
+elif(var==1):
+	print("x is equal to 3")
+elif(var==2):
+	print("x is equal to 2")
+	
+
+>>> def display(var):
+	if(var==0):
+	print("x is equal to zero")
+elif(var==1):
+	print("x is equal to 3")
+elif(var==2):
+	print("x is equal to 2")
+	
+
+>>> def display(var):
+	if(var==0):
+		print("x is equal to zero")
+elif(var==1):
+	print("x is equal to 3")
+elif(var==2):
+	print("x is equal to 2")
+	
+
+>>> def display(var):
+	if(var==0):
+		print("x is equal to zero")
+	elif(var==1):
+		print("x is equal to 1")
+	elif(var==2):
+		print("x is equal to 2")
+
+		
+>>> display(0)
+x is equal to zero
+>>> display(1)
+x is equal to 1
+>>> display(2)
+x is equal to 2
+>>> def show(var):
+	if(var==1):
+		print("value is equal to 1)
+		      
+
+>>> def show(var):
+	if(var==1):
+		print("value is equal to 1")
+	elif(var==2):
+		print("value is equal to 2")
+	elif(var==3):
+		print("value is equal to 3")
+
+		
+>>> show(1)
+value is equal to 1
+>>> show(2)
+value is equal to 2
+>>> show(3)
+value is equal to 3
+>>> def display(a,b=6):
+	self.a=self.a
+	self.b=self.b
+
+	
+>>> display(2)
+
+>>> def display(a,b=6):
+	print(a)
+	print(b)
+
+	
+>>> display(2)
+2
+6
+>>> def display(a,b="python"):
+	print(a)
+	print("python")
+
+	
+>>> display("java")
+java
+python
+>>> class employee:
+
+	inr=1.4
+	
+	def __init__(self,name,salary,place):
+		self.name=name
+		self.salary=salary
+		self.place=place
+		
+	def increament(self):
+		self.salary=int(self.salary*self.inr)
+
+		
+>>> emp=employee("migen",20000,"sankari")
+>>> emp.name
+'migen'
+>>> emp.place
+'sankari'
+>>> emp.salary
+20000
+>>> emp.inr
+1.4
+>>> emp.salary
+28000
+>>> emp.increament()
+>>> emp.salary
+39200
+>>> 
