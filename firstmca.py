04a3420c43a879b393069508e8bf815a00d91891
Priya Ravi <34087852+PriyaRaviit3@users.noreply.github.com>
2018-02-27 13:28:15 +0530

    Add files via upload

---
+++ firstmca.py
@@ -0,0 +1,7 @@
+class student:
+    def display(self,x):
+        self.x=x
+        print("{} hi display" .format(self.x))
+    def show(self,y):
+        self.y=y
+        print("%s hi show" % self.y)
