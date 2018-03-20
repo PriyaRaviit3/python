5e31cd9bdc77ed6a2f40fa84e284acb9fcbec6a6
Priya Ravi <34087852+PriyaRaviit3@users.noreply.github.com>
2018-03-20 14:12:20 +0530

    Update regular expression using search.py

--- regular expression using search.py
+++ regular expression using search.py
@@ -8,3 +8,20 @@ if(re.search("happy","smile it gives happiness...happy")):
 output:
 
 happy found
+
+
+
+
+
+
+import re
+myphone="98-94-69-9690"
+if re.search(r"\w{3},\w{2},\w{2},\w{4}",myphone):
+    print("valid")
+else:
+    print("not valid")
+
+
+output:
+
+not valid
