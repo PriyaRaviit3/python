cdad1360d2641ee9aa4d7f2f8d4b2808a28ae22a
Priya Ravi <34087852+PriyaRaviit3@users.noreply.github.com>
2018-03-20 13:22:15 +0530

    Update regular expression using findall.py

--- regular expression using findall.py
+++ regular expression using findall.py
@@ -5,5 +5,37 @@ for i in allhappy:
 
 
 output:
+    
 happy
 happy
+
+
+
+
+
+
+import re
+month="jm,fm,mm,am,mm,jm,am,sm,am,nm,dm"
+months=re.findall("[a-z]m",month)
+for i in months:
+    print(i)
+    
+    
+    
+    
+    output:
+        
+        jm
+        fm
+        mm
+        am
+        mm
+        jm
+        am
+        sm
+        am
+        nm
+        dm
+
+        
+        
