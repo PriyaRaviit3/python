2035b5a2845ffb891eadb1a8f7fe589158cd611a
Priya Ravi <34087852+PriyaRaviit3@users.noreply.github.com>
2018-03-23 13:11:15 +0530

    Update regular expression text match.py

--- regular expression text match.py
+++ regular expression text match.py
@@ -70,3 +70,26 @@ print(text_match("Aaab_abbbc"))
 found a match
 not match
 not match
+
+
+
+
+
+import re
+pattern = 'fox'
+text = 'The quick brown fox jumps over the lazy dog.'
+match = re.search(pattern, text)
+s = match.start()
+e = match.end()
+print('Found "%s" in "%s" from %d to %d ' % \
+    (match.re.pattern, match.string, s, e))
+
+
+
+
+#output:
+
+
+
+Found "fox" in "The quick brown fox jumps over the lazy dog." from 16 to 19 
+
