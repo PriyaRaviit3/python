c3491dbb2b56f0b08e8162f537f765b9321d0318
Priya Ravi <34087852+PriyaRaviit3@users.noreply.github.com>
2018-03-23 12:31:28 +0530

    Add files via upload

---
+++ text match.py
@@ -0,0 +1,25 @@
+http://regexlib.com/CheatSheet.aspx
+text match
+
+
+
+import re
+def text_match(text):
+    pattern="ab*?"
+
+    if re.search(pattern,text):
+        return"found a match"
+    else:
+        return"not match"
+print(text_match("ca"))
+print(text_match("abc"))
+print(text_match("abbc"))
+
+
+
+#output:
+
+
+found a match
+found a match
+found a match
