7c58311a11e2117dbad73f28dedca94019f04f7d
Priya Ravi <34087852+PriyaRaviit3@users.noreply.github.com>
2018-03-23 13:03:44 +0530

    Update regular expression text match.py

--- regular expression text match.py
+++ regular expression text match.py
@@ -47,3 +47,26 @@ print(text_match("abbc"))
 not match
 not match
 not match
+
+
+
+
+import re
+def text_match(text):
+    pattern="^[a-z]+_[a-z]+$"
+
+    if re.search(pattern,text):
+        return"found a match"
+    else:
+        return"not match"
+print(text_match("aab_cbbbc"))
+print(text_match("aab_Abbbc"))
+print(text_match("Aaab_abbbc"))
+
+
+#output:
+
+
+found a match
+not match
+not match
