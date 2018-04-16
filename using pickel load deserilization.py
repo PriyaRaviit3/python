f49401b48f1f262b6c9f5d6b6440c533d7222ceb
Priya Ravi <34087852+PriyaRaviit3@users.noreply.github.com>
2018-04-16 12:50:48 +0530

    Add files via upload

---
+++ using pickel load deserilization.py
@@ -0,0 +1,29 @@
+import pickle
+
+
+class student:
+    def __init__(self, name, age, rollno):
+        self.name = name
+        self.age = age
+        self.rollno = rollno
+
+    def display(self):
+        print("my name is %s = " % self.name)
+        print(" my age is %s= " % self.age)
+        print(" my rollno is %s= " % self.rollno)
+
+s = student("migen", 3, 6)
+
+
+with open("e:\green java\data2.pkl","wb") as data:
+        pickle.dump(s, data)
+
+
+with open("e:\green java\data2.pkl","rb") as data:
+        print(pickle.load(data))
+
+
+
+output:
+
+<__main__.student object at 0x0527B4F0>
