fd8c6e9cfc8de9b874d57266116cc0e1f722d760
Priya Ravi <34087852+PriyaRaviit3@users.noreply.github.com>
2018-04-18 11:45:47 +0530

    Update numpy--array.py

--- numpy--array.py
+++ numpy--array.py
@@ -1,12 +1,5 @@
  import numpy as np
 
-x=np.array([[1,2,3]],[4,5,6]])
-  File "<ipython-input-2-0b49232510d7>", line 1
-    x=np.array([[1,2,3]],[4,5,6]])
-                                ^
-SyntaxError: invalid syntax
-
-
 x=np.array([[1,2,3],[4,5,6]])
 
 print(x)
